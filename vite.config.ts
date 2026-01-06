336fd0f2d2d727b029e54f2e0a87a3b7da6ffd1d
Alejandro M. Ravasio <alejandroravasio@gmail.com>
2026-01-06 04:58:47 +0000

    fix: build content script as iife

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,43 @@
 import { defineConfig } from "vite"
 import { resolve } from "path"
 
-export default defineConfig({
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-    sourcemap: false,
-    rollupOptions: {
-      input: {
-        background: resolve(__dirname, "src/background/serviceWorker.ts"),
-        content: resolve(__dirname, "src/content/content.ts"),
-        options: resolve(__dirname, "src/options/options.ts"),
-      },
-      output: {
-        entryFileNames: "[name].js",
-        chunkFileNames: "chunks/[name].js",
-        assetFileNames: "assets/[name][extname]",
-        manualChunks: undefined,
+const outDir = "dist"
+
+export default defineConfig([
+  {
+    build: {
+      outDir,
+      emptyOutDir: true,
+      sourcemap: false,
+      rollupOptions: {
+        input: {
+          background: resolve(__dirname, "src/background/serviceWorker.ts"),
+          options: resolve(__dirname, "src/options/options.ts"),
+        },
+        output: {
+          entryFileNames: "[name].js",
+          chunkFileNames: "chunks/[name].js",
+          assetFileNames: "assets/[name][extname]",
+          manualChunks: undefined,
+        },
       },
     },
   },
-})
+  {
+    publicDir: false,
+    build: {
+      outDir,
+      emptyOutDir: false,
+      sourcemap: false,
+      rollupOptions: {
+        input: {
+          content: resolve(__dirname, "src/content/content.ts"),
+        },
+        output: {
+          entryFileNames: "[name].js",
+          format: "iife",
+        },
+      },
+    },
+  },
+])
